73ea172037b3627e76c8cc29695ef1518dda075d
pieter365 <pieterp365@gmail.com>
2023-02-10 14:51:56 +0000

    Update routes.php
    Update v1.0

--- routes.php
+++ routes.php
@@ -1,5 +1,6 @@
 <?php
 
+//Laravel route legacy work with Middleware
 Route::middleware('legacy')
     ->any('/{path?}', ['as' => 'legacy', 'uses' => '\Legacy\Controller@index'])
     ->where('path', '.*');
