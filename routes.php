9e088e59f85a45b9f53b8df78fb36fccd68865a6
pieter365 <pieterp365@gmail.com>
2023-02-13 10:09:35 +0000

    Delete routes.php

--- routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-//Laravel route legacy work with Middleware
-Route::middleware('legacy')
-    ->any('/{path?}', ['as' => 'legacy', 'uses' => '\Legacy\Controller@index'])
-    ->where('path', '.*');
